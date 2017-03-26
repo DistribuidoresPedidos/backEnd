c9543a9fab2a49f0a58db20808d6f7650f587297
Unknown <lacardenasv@unal.edu.co>
2017-03-26 10:08:30 -0500

    Migrations and fix the orderProduct table

--- db/migrate/20170326142804_create_order_products.rb
+++ db/migrate/20170326142804_create_order_products.rb
@@ -3,7 +3,7 @@ class CreateOrderProducts < ActiveRecord::Migration[5.0]
     create_table :order_products do |t|
       t.float :quantity, :null=>false
       t.float :price, :null=>false
-      t.references :offeredProduct, foreign_key: true
+      t.references :offered_product, foreign_key: true
       t.references :order, foreign_key: true
 
       t.timestamps
