2a5a71b4df558827dd1558602d5bb56527cf76f2
Laura Cardenas <lacardenasv@unal.edu.co>
2017-03-28 18:53:05 -0500

    Strong params in aplication_controller for device_controller for sig_up and update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::API
+  #devise es celoso parametros fuertes 'strong params' adicionalmente vamos a pasar mas llaves 
+  #sainitizer no deja modificar 
+  
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :configure_permitted_parameters if :devise_controller?
+  def :configure_permitted_parameters
+    if resource_class== Retailer 
+      devise_parameter_sanitezer.permit(:sign_up, keys: [:name, :description, :email, :phoneNumber, :photo, :location])
+      devise_parameter_sanitezer.permit(:account_update, keys: [:name, :description, :phoneNumber, :photo])
+    elsif resource_class== Distributor
+      devise_parameter_sanitezer.permit(:sign_up, keys: [:name, :phoneNumber, :photo, :email, :location])
+      devise_parameter_sanitezer.permit(:account_update, keys: [:name, :phoneNumber, :photo])
+    end
+  end
 end
