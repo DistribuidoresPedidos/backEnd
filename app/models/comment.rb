8ff305ef3d88f08b90e2dcd9a39fb17b057cc60d
Laura Cardenas <lacardenasv@unal.edu.co>
2017-03-30 14:33:26 -0500

    Querys comment and order

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,12 +4,11 @@ class Comment < ApplicationRecord
   validates :content , length: { minimum:7 }  
   validates :calification , numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5, only_integer: true }
 
-end
-
-def self.comments_by_distributor(distributor, page=1, per_page=>10)
-  .includes(order: {route: :distributor})
-    .where(route:{
-      distributor_id: distributor 
-    }).paginate(:page=> page , :per_page=> per_page)
-    
+  def self.comments_by_distributor(distributor, page=1, per_page=>10)
+    .includes(order: {route: :distributor})
+      .where(route:{
+        distributor_id: distributor 
+      }).paginate(:page=> page , :per_page=> per_page)
+      
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,47 @@ class Order < ApplicationRecord
   validates :state , :exitDate , :arrivalDate , presence: true
   validates :totalPrice ,numericality: true, presence: true
 
+
+#ask the attribute
+scope :delivered where(state: 'deliver')
+
+def self.load_orders(page=1, per_page=10)
+    includes(:retailer, :route)
+    .paginate(:page => page, :per_page => per_page)
+end 
+#retrieve a order
+def self.order_by_id(id){
+  includes(:retailer, :route)
+    .find_by_id(id)
+  }
 end
+#retrieve all retailer's shop --Aks deliveres instead of self? 
+def self.load_order_by_retailer(retailer, page=1, per_page=10)
+  delivered.includes(:retailer)
+    .where( orders:{
+      retailer_id: retailer
+    }).paginate(:page=> page, :per_page=> per_page)
+  end
+
+#retrive all distributor's sell --Aks delivered
+
+  def self.load_order_by_distributor(distributor, page=1, per_page=10)
+    delivered.includes(:route)
+  end
+
+def self.order_by_arrivDate(date, page=1, per_page=10)
+  load_orders(page, per_page)
+    .where(arrivalDate: date)
+  end 
+
+
+def self.order_by_exitDate(date, page=1, per_page=10)
+  load_orders(page, per_page)
+    .where(exitDate: date)
+  end 
+
+  def self.order_by_route(route, page=1, per_page=10)
+    
+  end
+  def self.order_by_productOrder(orderProduct)
+end 
