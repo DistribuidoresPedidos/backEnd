19b647db83281577bfe0bfc75eb8f37327d4ef29
Laura Cardenas <lacardenasv@unal.edu.co>
2017-03-31 02:59:40 -0500

    /querys: Order

--- app/models/offered_product.rb
+++ app/models/offered_product.rb
@@ -3,4 +3,9 @@ class OfferedProduct < ApplicationRecord
   belongs_to :distributor
   has_many :orderProducts
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
+
+  def self.loads_offeredProduct()
+    includes(:product, :distributor, :orderProducts, :price)
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -11,44 +11,56 @@ class Order < ApplicationRecord
 scope :delivered where(state: 'delivered')
 
 def self.load_orders(page=1, per_page=10)
-    includes(:retailer, :route)
+    includes(:retailer, :route, :orderProducts, :comments)
     .paginate(:page => page, :per_page => per_page)
 end 
 #retrieve a order
-def self.order_by_id(id){
-  includes(:retailer, :route)
+def self.order_by_id(id)
+  includes(:retailer, :route,:orderProducts, :comments)
     .find_by_id(id)
-  }
+  
 end
 #retrieve all retailer's shop --Aks deliveres instead of self? 
 def self.delivered.load_order_by_retailer(retailer, page=1, per_page=10)
-  includes(:retailer)
+  load_orders(page, per_page)
     .where( orders:{
       retailer_id: retailer
     }).paginate(:page=> page, :per_page=> per_page)
-  end
+ end
 
 #retrive all distributor's sell --Aks delivered
 
 
   def self.delivered.load_order_by_distributor(distributor, page=1, per_page=10)
-    includes(:route)
+    load_orders(page, per_page)
+    .where(routes:{
+      distributor_id: distributor 
+    }).paginate(:page=> page, :per_page=> per_page)
 
   end
 
 def self.order_by_arrivDate(date, page=1, per_page=10)
   load_orders(page, per_page)
-    .where(arrivalDate: date)
+    .where( orders:{
+      arrivalDate: date
+    })
   end 
 
 
 def self.order_by_exitDate(date, page=1, per_page=10)
   load_orders(page, per_page)
-    .where(exitDate: date)
+    .where(orders:{
+      exitDate: date 
+    })
   end 
 
   def self.order_by_route(route, page=1, per_page=10)
-    
+    load_orders(page, per_page)
+    .where(orders:{
+      route_id: route
+    }).paginate(:page=> page, :per_page=> per_page)
   end
-  def self.order_by_productOrder(orderProduct)
+  def self.order_by_orderProduct(orderProduct)
+    joins()
+  end
 end 
