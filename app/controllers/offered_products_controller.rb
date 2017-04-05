08dac98ecf806b91d92aff9b5cb55027ce939e5e
Laura Cardenas <lacardenasv@unal.edu.co>
2017-04-05 01:57:55 -0500

    offered_product controller

--- app/controllers/offered_products_controller.rb
+++ app/controllers/offered_products_controller.rb
@@ -1,22 +1,16 @@
 class OfferedProductsController < ApplicationController
-  before_action :set_offered_product, only: [:show, :update, :destroy]
+  before_action :set_offered_product, only: [:index,:show, :update, :destroy]
 
   # GET /offered_products
   def index
-    
-    if params.has_key?(:distributor_id)
-      @offered_products = OfferedProduct.offered_products_by_distributor(params[:distributor_id],@page,@per_page)  
-    else
-      @products = Product.load_products(@page,@per_page)
-    end
-    @offered_products = OfferedProduct.all
 
-    render json: @offered_products
+    render json: @offered_products, status: :ok
+  
   end
 
   # GET /offered_products/1
   def show
-    render json: @offered_product
+    render json: @offered_product, status: :ok
   end
 
   # POST /offered_products
@@ -33,7 +27,7 @@ class OfferedProductsController < ApplicationController
   # PATCH/PUT /offered_products/1
   def update
     if @offered_product.update(offered_product_params)
-      render json: @offered_product
+      render json: @offered_product, status: :ok
     else
       render json: @offered_product.errors, status: :unprocessable_entity
     end
@@ -44,16 +38,36 @@ class OfferedProductsController < ApplicationController
     @offered_product.destroy
   end
 
+  def offered_products_by_categories
+    @offered_products = OfferedProduct.offered_products_by_categories(params[:categories])
+    
+    render json: @offered_products , status: :ok
+  end
+
+  def suggest_to_retailer
+    @offered_products= OfferedProduct.suggest_to_retailer(params[:retailer_id])
+    render json: @offered_products , status: :ok
+  end
+
+  def offered_products_by_param_retailer
+    @offered_products= OfferedProduct.offered_products_by_param_retailer(params[:name], params[:retailer_id])
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_offered_product
 
-
-      @offered_product = OfferedProduct.find(params[:id])
+      if params.has_key?(:distributor_id)
+        @offered_products = OfferedProduct.offered_products_by_distributor(params[:distributor_id],params[:page], params[:per_page])
+      elsif params.has_key?(:retailer_id)
+        @offered_products =  OfferedProduct.offered_products_by_retailer(params[:retailer_id],params[:page], params[:per_page])
+      else
+        @offered_products =  OfferedProduct.load_offered_products(params[:page], params[:per_page])
+      end
+      @offered_product = @offered_products.offered_product_by_id(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def offered_product_params
-      params.require(:offered_product).permit(:price, :product_id, :distributor_id)
+      params.require(:offered_product).permit(:price, :product_id, :distributor_id, :categories, :name, :retailer_id)
     end
 end
