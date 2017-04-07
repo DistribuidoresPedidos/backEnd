abeaad3053e472c522f311b69308e924511fab47
Juan Rubio <jcrubioa@unal.edu.co>
2017-04-06 21:12:29 -0500

    Routes Working, devise failing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 #Retailer dont has routes for products , and retailer has routes for offered_products
-  namespace :api, defaults: {format: :json} do
-    namespace :v1 do
+  
+  scope '/api' do
+    scope '/v1' do
        mount_devise_token_auth_for 'Distributor', at: 'distri_path'
        as :distributor do
         resources :distributors, only: [:index, :show] do
@@ -18,14 +19,15 @@ Rails.application.routes.draw do
         resources :retailers, only: [:index, :show] do
           get 'orders_by_arrival_date', to: 'orders#orders_by_arrival_date'
           get 'orders_by_exit_date', to: 'orders#orders_by_exit_date'
-          resources :orders
-          resources :orders
+          resources :orders, except: [:create]
           resources :offered_products
         end
       end
 
-  resources :orders do
-    resources :comments
-  end
+      resources :orders do
+        resources :comments
+      end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    end
+  end
 end
