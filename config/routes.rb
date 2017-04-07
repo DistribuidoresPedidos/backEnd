670bacf81c1038fa6d30e3815cecf0fadb37ac37
Laura Cardenas <lacardenasv@unal.edu.co>
2017-04-06 23:59:27 -0500

    more routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
         resources :distributors, only: [:index, :show] do
           get 'orders_by_arrival_date', to: 'orders#orders_by_arrival_date'
           get 'orders_by_exit_date', to: 'orders#orders_by_exit_date'
-          get 'offered_products_by_categories', to: 'offered_products#offered_products_by_categories'
-          get 'products_by_categories', to: 'products#products_by_categories'
           resources :orders, except: [:create]
           resources :products 
           resources :offered_products
@@ -22,10 +20,10 @@ Rails.application.routes.draw do
         resources :retailers, only: [:index, :show] do
           get 'orders_by_arrival_date', to: 'orders#orders_by_arrival_date'
           get 'orders_by_exit_date', to: 'orders#orders_by_exit_date'
-          get 'offered_products_by_categories', to: 'offered_products#offered_products_by_categories'
+          
           get 'suggest_to_retailer', to: 'offered_products#suggest_to_retailer'
           get 'offered_products_by_retailer', to: 'offered_products#offered_products_by_param_retailer'
-          get 'products_by_categories', to: 'products#products_by_categories'
+          
           get 'distributors_by_retailer', to: 'distributors#distributors_by_retailer'
           get 'order_product_by_retailer', to: 'order_products#order_product_by_retailer' 
           resources :orders, except: [:create]
@@ -33,14 +31,20 @@ Rails.application.routes.draw do
         end
       end
  
-  resources :routes do  
-    resources :orders 
-    resources :coordinates 
-  end  
-       
-  resources :offered_products do  
-    get 'coordinate_by_offered_product', to: 'coordinates#coordinate_by_offered_product'  
-  end 
+      resources :routes do  
+        resources :orders 
+        resources :coordinates 
+      end  
+          
+      resources :offered_products do
+        get 'offered_products_by_categories', to: 'offered_products#offered_products_by_categories'  
+        get 'coordinate_by_offered_product', to: 'coordinates#coordinate_by_offered_product'  
+      end 
+      
+      resources :products do
+        get 'products_by_categories', to: 'products#products_by_categories'
+        get 'products_by_ids', to: 'products#products_by_ids'
+      end
 
       resources :orders do
         resources :comments
