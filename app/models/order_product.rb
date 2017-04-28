c561f9011215581c787a2f5cf9dbbbbcfce1424b
Juan Rubio <jcrubioa@unal.edu.co>
2017-04-27 22:27:15 -0500

    Problemas con último merge

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -4,15 +4,10 @@ class OrderProduct < ApplicationRecord
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
   
-  def self.load_OrderProducts(page=1, per_page=10)
-    	includes(orders:[:orderProducts])
+  def self.load_orderProducts(page=1, per_page=10)
+    	includes(:offered_product, :order)
       .paginate(:page => page, :per_page => per_page)
   end
-
-  def self.load_orderProduct(page=1, per_page=10)
-    includes(:offeredProduct, :order)
-    .paginate(:page => page, :per_page => per_page)
-  end 
 
   def self.orderProduct_by_distributor(distributor, page=1, per_page=10)
     includes(order: {route: :distributor})
@@ -21,18 +16,18 @@ class OrderProduct < ApplicationRecord
     }).paginate(:page=> page, :per_page=> per_page)
   end
 
-  def self.orderProduct_by_retailer(retailer, page=1, per_page=10)
-    load_orderProduct(page, per_page)
+   def self.orderProduct_by_retailer(retailer, page=1, per_page=10)
+    load_orderProducts(page, per_page)
     .where(orders:{
         retailer_id: retailer 
     }).paginate(:page => page, :per_page => per_page)
   end
 
-  def self.orderProduct_by_order(order)
-    load_orderProduct(page, per_page)  
+  def self.order_products_by_order(order, page=1, per_page=10)
+    load_orderProducts(page, per_page)  
     .where(order_products:{
       order_id: order
-    })
+    }).paginate(:page => page, :per_page => per_page)
   end
 
   def self.orderProduct_by_route( route, page=1, per_page=10)
