d26d794481a179ffb3cc6475289684d8f4ba9d21
Juan Rubio <jcrubioa@unal.edu.co>
2017-05-01 18:45:51 -0500

    done

--- app/models/offered_product.rb
+++ app/models/offered_product.rb
@@ -79,6 +79,16 @@ end
     })
   end
 
+  def self.offered_products_by_param_match_all(param, min_price, max_price)
+    includes(:product, distributor:{routes: :coordinates})
+    .where(products:{
+      id: (Product.search param, fields: [:name], match: :word_middle, misspellings: {below: 5}).pluck(:id)
+    },
+    offered_products:{
+      price: min_price..max_price
+    })
+  end
+
   def self.simple_search(param, category)
       Product.search param, aggs: [category], fields: [:name], match: :word_middle, misspellings: {below: 5}
   end
@@ -119,7 +129,11 @@ end
   def self.offered_products_by_param_retailer_match(param, retailer_id, categories, min_price, max_price, page=1, per_page=10)
     s1 = Set.new
     retailer = Retailer.retailer_by_id(retailer_id)
-    possible_offered_product = offered_products_by_param_match(param, categories, min_price, max_price)
+    if categories != '*'
+      possible_offered_product = offered_products_by_param_match(param, categories, min_price, max_price)
+    else
+      possible_offered_product = offered_products_by_param_match_all(param, min_price, max_price)
+    end
     possible_offered_product.each do |i|
       coordinates = Coordinate.find_by_offered_product(i)
       c = coordinates.within_radius(20000, retailer.latitude, retailer.longitude)
